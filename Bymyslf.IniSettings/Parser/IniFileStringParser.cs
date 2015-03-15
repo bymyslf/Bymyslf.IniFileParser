1abd7cb1c13c3aa4bdab763eedb0c186ac5c24c7
Luís Barbosa <luisbarbosa@outlook.com>
2015-03-15 20:30:51 +0000

    Added IniFileStringParser

---
+++ Bymyslf.IniSettings/Parser/IniFileStringParser.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Bymyslf.IniSettings.Parser
+{
+    public class IniFileStringParser : IniFileParserBase, IIniFileStringParser
+    {
+        public IniFile Parse(string iniFileString)
+        {
+            if (String.IsNullOrEmpty(iniFileString))
+            {
+                throw new ArgumentException("iniFileString cannot be null or empty!");
+            }
+
+            IniFile file = new IniFile();
+
+            commentsListAux.Clear();
+            sectionAux = null;
+
+            var lines = iniFileString.Split(Environment.NewLine.ToCharArray(), StringSplitOptions.RemoveEmptyEntries);
+            foreach (var currentLine in lines)
+            {
+                ReadLine(currentLine, file);
+            }
+
+            return file;
+        }
+    }
+}
