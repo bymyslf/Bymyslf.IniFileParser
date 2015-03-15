cf0a51a00637f50815bc56e6247b42e5df7c7ff7
Luís Barbosa <luisbarbosa@outlook.com>
2015-03-15 20:42:46 +0000

    Added IniFileInMemoryReader

---
+++ Bymyslf.IniSettings/IO/IniFileInMemoryReader.cs
@@ -0,0 +1,58 @@
+﻿using Bymyslf.IniSettings.Parser;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Bymyslf.IniSettings.IO
+{
+    public class IniFileInMemoryReader : IIniFileReader
+    {
+        private readonly string filePath;
+        private readonly IIniFileStringParser parser;
+
+        public IniFileInMemoryReader(string path)
+            : this(path, new IniFileStringParser())
+        {
+        }
+
+        public IniFileInMemoryReader(string path, IIniFileStringParser parser)
+        {
+            if (String.IsNullOrEmpty(path))
+            {
+                throw new ArgumentException("File path cannot be null or empty!");
+            }
+
+            if (parser == null)
+            {
+                throw new ArgumentNullException("Parser cannot be null!");
+            }
+
+            this.filePath = path;
+            this.parser = parser;
+        }
+
+        public IniFile Read()
+        {
+            return Read(Encoding.Default);
+        }
+
+        public IniFile Read(Encoding fileEncoding)
+        {
+            try
+            {
+                using (FileStream fs = File.Open(filePath, FileMode.Open, FileAccess.Read, FileShare.ReadWrite))
+                {
+                    using (StreamReader sr = new StreamReader(fs, fileEncoding))
+                    {
+                        return parser.Parse(sr.ReadToEnd());
+                    }
+                }
+            }
+            catch (IOException ex)
+            {
+                throw;
+            }
+        }
+    }
+}
