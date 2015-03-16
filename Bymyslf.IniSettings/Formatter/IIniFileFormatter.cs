46ddc8f5efd34f308751b89f04451506578c1a6a
Luís Barbosa <luisbarbosa@outlook.com>
2015-03-16 11:51:29 +0000

    Implemented IniFileStreamWriter Write

--- Bymyslf.IniSettings/Formatter/IIniFileFormatter.cs
+++ Bymyslf.IniSettings/Formatter/IIniFileFormatter.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Bymyslf.IniSettings.Formatter
+﻿namespace Bymyslf.IniSettings.Formatter
 {
     public interface IIniFileFormatter
     {
--- Bymyslf.IniSettings/IO/IniFileStreamWriter.cs
+++ Bymyslf.IniSettings/IO/IniFileStreamWriter.cs
@@ -7,19 +7,19 @@ namespace Bymyslf.IniSettings.IO
 {
     public class IniFileStreamWriter : IIniFileWriter
     {
-        private readonly string path;
+        private readonly string filePath;
         private readonly IIniFileFormatter formatter;
 
-        public IniFileStreamWriter(string path)
-            : this(path, new IniFileFormatter())
+        public IniFileStreamWriter(string filePath)
+            : this(filePath, new IniFileFormatter())
         {
         }
 
-        public IniFileStreamWriter(string path, IIniFileFormatter formatter)
+        public IniFileStreamWriter(string filePath, IIniFileFormatter formatter)
         {
-            if (String.IsNullOrEmpty(path))
+            if (String.IsNullOrEmpty(filePath))
             {
-                throw new ArgumentException("path cannot be null or empty");
+                throw new ArgumentException("filePath cannot be null or empty");
             }
 
             if (formatter == null)
@@ -27,18 +27,31 @@ namespace Bymyslf.IniSettings.IO
                 throw new ArgumentNullException("formatter cannot be null");
             }
 
-            this.path = path;
+            this.filePath = filePath;
             this.formatter = formatter;
         }
 
         public void Write(IniFile iniFile)
         {
-            throw new NotImplementedException();
+            Write(iniFile, Encoding.Default);
         }
 
         public void Write(IniFile iniFile, Encoding fileEncoding)
         {
-            throw new NotImplementedException();
+            try
+            {
+                using (FileStream fs = File.Open(filePath, FileMode.Create, FileAccess.Write))
+                {
+                    using (StreamWriter sw = new StreamWriter(fs, fileEncoding))
+                    {
+                        sw.Write(formatter.Format(iniFile));
+                    }
+                }
+            }
+            catch (IOException ex)
+            {
+                throw;
+            }
         }
     }
 }
