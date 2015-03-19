66d18e4efb2e3080b08cad8ba79288d7d44c9b17
Luís Barbosa <luisbarbosa@outlook.com>
2015-03-19 10:19:41 +0000

    Added IniFileParseException

---
+++ Bymyslf.IniFileParser/Exceptions/IniFileParseException.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Bymyslf.IniFileParser.Exceptions
+{
+    public class IniFileParseException : Exception
+    {
+        public IniFileParseException()
+        {
+        }
+
+        public IniFileParseException(string message)
+            : base(message)
+        {
+        }
+
+        public IniFileParseException(string message, Exception inner)
+            : base(message, inner)
+        {
+        }
+    }
+}
